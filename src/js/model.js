093c10aa5241dd19a11cb410ad1bae6081b9e0da
Bb <bncblnc@gmail.com>
2022-09-02 16:23:41 -0300

    pagination

--- src/js/model.js
+++ src/js/model.js
@@ -1,12 +1,14 @@
 import { async } from 'regenerator-runtime/runtime';
-import { API_URL } from './config';
+import { API_URL, RES_PER_PAGE } from './config';
 import { getJSON } from './helpers';
 
 export const state = {
   recipe: {},
   search: {
     query: '',
+    page: 1,
     results: [],
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -51,4 +53,13 @@ export const loadSearchResults = async function (query) {
     console.error(`${err.message} 💥💥💥💥`);
     throw err;
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+
+  return state.search.results.slice(start, end);
 };
