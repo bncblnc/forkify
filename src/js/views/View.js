3124b882bf991bf6e1e220bf6aa3caea64ef6d4b
Bb <bncblnc@gmail.com>
2022-09-12 11:51:53 -0300

    deploy test2

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -2,6 +2,7 @@ import icons from 'url:../../img/icons.svg';
 
 export default class View {
   _data;
+  _parentElement;
 
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
